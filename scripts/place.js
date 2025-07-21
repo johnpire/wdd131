daa70e95a34ff2bdbee5398f43c2cfc81a08ad35
johnpire <ijcwasbornonthe15thofseptember@gmail.com>
2025-07-21 18:51:32 +0800

    W03 Assignment: Country Page

---
+++ scripts/place.js
@@ -0,0 +1,23 @@
+const today = new Date();
+const year = document.querySelector("#currentyear");
+year.textContent = today.getFullYear();
+
+const lastModified = document.querySelector("#lastModified");
+lastModified.textContent = `Last Modification: ${document.lastModified}`;
+
+let temperature = -10;
+let windSpeed = 15;
+let conditions = "Light Snow";
+
+let windChill = 0;
+function calculateWindChill(temp, windSpeed)
+{
+    return windChill = 13.12 + 0.6215 * temp - 11.37 * (windSpeed ** 0.16) + 0.3965 * temp * (windSpeed ** 0.16)
+}
+
+windChill = calculateWindChill(temperature, windSpeed);
+
+document.getElementById("temperature").textContent = `${temperature} °C`;
+document.getElementById("conditions").textContent = conditions;
+document.getElementById("windSpeed").textContent = `${windSpeed} km/h`;
+document.getElementById("windChill").textContent = `${windChill.toFixed(1)} °C`;
